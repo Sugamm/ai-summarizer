53a49069ed5fa5adad3e8d077e258583f6457e83
Sugam <sugam@happylocate.com>
2023-05-12 02:41:43 +0530

    added a github link

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -6,7 +6,7 @@ const Hero = () => {
     <header className='w-full flex justify-center items-center flex-col'>
       <nav className='flex justify-between items-center w-full mb-10 pt-3'>
         <img src={logo} alt="sumz_logo" className='w-28 object-contain' />
-        <button type='button' onClick={() => window.open(".")} className='black_btn' 
+        <button type='button' onClick={() => window.open("https://github.com/Sugamm/ai-summarizer")} className='black_btn' 
         >
           Github
         </button>
